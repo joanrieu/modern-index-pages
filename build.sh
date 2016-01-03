59364c553fedffc4d467b8e28d87fd5190f9ea5b
Joan Rieu <joan.rieu@gmail.com>
2016-01-03 15:38:00 +0000

    Added subdirectories

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 
 TARGET=modern-index-pages
 
-mkdir $TARGET/
-webpack --entry ./index.js --output-path $TARGET/ --output-filename bundle.js
-cp icon128.png listener.js manifest.json style.css $TARGET/
+rm -rf $TARGET{/,.zip}
+
+cp -R src/ $TARGET/
+webpack \
+    --entry ./src/index.js \
+    --output-path $TARGET/ \
+    --output-filename index.js \
+    --bail
 zip $TARGET.zip -r $TARGET/
 rm -rf $TARGET/
--- src/listener.js
+++ src/listener.js
@@ -3,7 +3,7 @@ chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
         chrome.tabs.get(details.tabId, function(tab) {
             if (tab.title.indexOf("Index of ") === 0) {
                 chrome.tabs.insertCSS(details.tabId, { file: "style.css" });
-                chrome.tabs.executeScript(details.tabId, { file: "bundle.js" });
+                chrome.tabs.executeScript(details.tabId, { file: "index.js" });
             }
         });
     } catch (e) {
